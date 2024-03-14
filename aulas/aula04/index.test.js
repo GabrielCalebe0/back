2916501a171c6be8a86238169c8e3b63c03c227d
Gabriel Calebe <gabrielcalebe007@gmail.com>
2024-03-14 10:15:11 -0300

    mudando o index.test.js

--- aulas/aula04/index.test.js
+++ aulas/aula04/index.test.js
@@ -1,3 +1,5 @@
+
+
 const supertest = require('supertest')
 
 const app = require('./index')
@@ -7,5 +9,20 @@ const request = supertest(app)
 test(" Deve retornar 200 no GET /", async function(){
     const response = await request.get("/")
     expect(response.status).toBe(200)
+
+})
+test(" Deve retornar 201 no POST /", async function(){
+    const response = await request.get("/")
+    expect(response.status).toBe(201)
+
+})
+test(" Deve retornar 200 no PUT /", async function(){
+    const response = await request.get("/")
+    expect(response.status).toBe(200)
+
+})
+test(" Deve retornar 200 no DELETE /", async function(){
+    const response = await request.get("/")
+    expect(response.status).toBe(204)
 
 })
